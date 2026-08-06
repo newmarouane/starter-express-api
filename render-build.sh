90a22fbd13a331f5012a68fe8323aeba0215a7a6
newmarouane <80288727+newmarouane@users.noreply.github.com>
2026-08-06 11:52:58 +0100

    Create render-build.sh

---
+++ render-build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y wget gnupg
+
+wget -qO- https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list
+
+apt-get update
+apt-get install -y google-chrome-stable
+
+npm install
