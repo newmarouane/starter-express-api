7186e94a08d4702804f7f4a202ebf3b2d028e4d2
newmarouane <80288727+newmarouane@users.noreply.github.com>
2026-08-06 10:45:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const viewport = {
   };
 
   const browser = await puppeteer.launch({
-  executablePath: "/usr/bin/google-chrome",
+ // executablePath: "/usr/bin/google-chrome",
   headless: true,
   args: [
     "--no-sandbox",
