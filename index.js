94c16f4931d8763b3f894ad9f07f19a0ad504678
newmarouane <80288727+newmarouane@users.noreply.github.com>
2026-08-07 11:01:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var cloudscraper = require('cloudscraper');
 const app = express()
 
 app.get("/", async (req, res) => {
-
+const isin='MA0000012528';
 const medias24ApiUrl = `https://medias24.com/content/api?method=getBidAsk&ISIN=${isin}&format=json`;
   
 	 const curlCommand = `curl -s "${medias24ApiUrl}" -H "Content-Type: application/json" --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36"`;
